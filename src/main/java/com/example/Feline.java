785c6a5fc7614a3dee218556fb18f9f60740d15b
Aleksandr Deltsov <adeltsovjob@yandex.ru>
2025-02-23 21:40:20 +0300

    update file

--- src/main/java/com/example/Feline.java
+++ src/main/java/com/example/Feline.java
@@ -4,9 +4,19 @@ import java.util.List;
 
 public class Feline extends Animal implements Predator {
 
+    Animal animal;
+
+    public Feline(){
+
+    }
+
+    public Feline(Animal animal) {
+        this.animal = animal;
+    }
+
     @Override
     public List<String> eatMeat() throws Exception {
-        return getFood("Хищник");
+        return animal.getFood("Хищник");
     }
 
     @Override
